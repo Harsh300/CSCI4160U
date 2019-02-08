2db7c3a2fb2855f0514fca9656b9b8808c1f1cba
Harshan <harshan.mohanaraj@uoit.net>
2019-02-07 22:50:49 -0500

    Changes

---
+++ Assignment1/Assets/PlayerController2D.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController2D : MonoBehaviour
+{
+
+
+    // Variables 
+
+    [Header("General")]
+    [SerializeField] private float movementSpeed = 10f;
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+    [SerializeField] private LayerMask groundLayers;
+    public float speed = 0.0f;
+
+    [Header("Jumping")]
+    [SerializeField] private bool canAirControl = true;
+    [SerializeField] private float jumpForce = 500f;
+    [SerializeField] private Transform groundPosition;
+    public bool isGrounded;
+
+
+
+    private bool isFacingRight = true;
+
+    const float groundedRadius = .2f;
+    const float ceilingRadius = .2f;
+
+    private Rigidbody2D rigidBody;
+    private Vector3 velocity = Vector3.zero;
+
+    [Header("Events")]
+    public UnityEvent OnLandEvent;
+
+
+
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+        {
+            OnLandEvent = new UnityEvent();
+        }
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        // Find any ground layer colliders closer than the ground position
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundPosition.position, groundedRadius, groundLayers);
+
+        Debug.Log("Overlapping colliders: " + colliders.Length);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+
+            // If any of the colliders are not the object iself, it must be the ground
+            if (colliders[i].gameObject != gameObject)
+            {
+                // Grounded
+                isGrounded = true;
+
+                // Not Grounded
+                if (!wasGrounded)
+                {
+                    OnLandEvent.Invoke();
+                }
+            }
+        }
+    }
+
+
+    // Moving and Jumping
+    public void Move(float move, bool jump)
+    {
+
+
+        // Control the player if grounded or canAirControl is turned on
+        if (isGrounded || canAirControl)
+        {
+
+
+
+            Vector3 targetVelocity = new Vector2(move * movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+            // Smoothing to the speed
+            rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+
+            // Getting the speed
+            speed = rigidBody.velocity.magnitude;
+
+            if (move > 0 && !isFacingRight)
+            {
+
+                // Flip the sprite horizontally when going right
+                Flip();
+            }
+            else if (move < 0 && isFacingRight)
+            {
+                // Flip the sprite horizontally when going left
+                Flip();
+            }
+        }
+        if (isGrounded && jump)
+        {
+
+            // add a vertical force to the player
+            isGrounded = false;
+            rigidBody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    // Flipping player depending if moving left or run
+    private void Flip()
+    {
+        // remember which way the sprite is facing
+        isFacingRight = !isFacingRight;
+
+        // multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+
+}
---
+++ Assignment1/Assets/PlayerInput.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+
+    // Variables 
+    public float runSpeed = 40f;
+
+    private PlayerController2D controller;
+    private Animator animator;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+
+    private void Start()
+    {
+
+        // Get Componenents
+        controller = GetComponent<PlayerController2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        // Horzonital Movement in direction of x
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        // Click Button to Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+        // Get Animator Parameters
+        animator.SetFloat("Speed", controller.speed);
+        animator.SetBool("IsJumping", !controller.isGrounded);
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move PLayer
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
