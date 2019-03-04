f9da35ccbde57e40f183aceba1add43c387a8ae2
Harshan <harshan.mohanaraj@uoit.net>
2019-03-04 16:30:40 -0500

    Project 1

---
+++ Project1/Assets/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 5f;
+    public int damage = 1;
+    public Rigidbody2D rb;
+ 
+    void Start()
+    {
+        rb.velocity = transform.right * speed;  
+    }
+    
+
+
+    private void onTriggerEnter2D(Collider2D colllider)
+    {
+        debug.Log("Destroyed object");
+        Destroy(gameObject);
+    }
+}
